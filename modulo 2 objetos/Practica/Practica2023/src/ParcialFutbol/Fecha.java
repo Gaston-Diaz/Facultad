212f74be1338e200b7f9843a68329f3c047338e1
gaston-diaz <gastondiaz.lp@gmail.com>
2023-10-15 08:30:01 -0300

    parcial futbol

---
+++ modulo 2 objetos/Practica/Practica2023/src/ParcialFutbol/Fecha.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ParcialFutbol;
+
+/**
+ *
+ * @author gasto
+ */
+public class Fecha {
+    private int cantGoleadores;
+    private Goleador[] goleadores;
+    
+    public Fecha(){}
+
+    public int getCantGoleadores() {
+        return cantGoleadores;
+    }
+
+    public void setCantGoleadores(int cantGoleadores) {
+        this.cantGoleadores = cantGoleadores;
+    }
+    
+    
+}
---
+++ modulo 2 objetos/Practica/Practica2023/src/ParcialFutbol/Goleador.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ParcialFutbol;
+
+/**
+ *
+ * @author gasto
+ */
+public class Goleador {
+    private String nombre;
+    private String nombreE;
+    private int goles;
+    
+    public Goleador(String unNombre, String unNombreE,int unosGoles){
+        setNombre(unNombre);
+        setNombreE(unNombreE);
+        setGoles(unosGoles);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombreE() {
+        return nombreE;
+    }
+
+    public void setNombreE(String nombreE) {
+        this.nombreE = nombreE;
+    }
+
+    public int getGoles() {
+        return goles;
+    }
+
+    public void setGoles(int goles) {
+        this.goles = goles;
+    }   
+    
+}
