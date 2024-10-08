be3984afb3fe536745e82ae1699a5474ba07e8e1
Gaston-Diaz <gastondiaz.lp@gmail.com>
2024-10-07 22:05:01 -0300

    p3e4a

--- 2-Objetos/Practica 1/Practica-2024/src/practica3/Cliente.java
+++ 2-Objetos/Practica 1/Practica-2024/src/practica3/Cliente.java
@@ -10,5 +10,48 @@ package practica3;
  * @author Usuario
  */
 public class Cliente {
+    private String nombre;
+    private int dni;
+    private int edad;
     
+    //constructores
+    public Cliente(){}
+    
+    public Cliente(String unNombre, int unDni, int unaEdad){
+        nombre = unNombre;
+        dni = unDni;
+        edad = unaEdad;
+    }
+    
+    //getter y setter
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    @Override
+    public String toString(){
+        String aux = " Nombre: " + getNombre() + " dni: " + getDni() + " edad: " + getEdad();
+        return aux;
+    }
 }
--- 2-Objetos/Practica 1/Practica-2024/src/practica3/Habitacion.java
+++ 2-Objetos/Practica 1/Practica-2024/src/practica3/Habitacion.java
@@ -4,11 +4,41 @@
  * and open the template in the editor.
  */
 package practica3;
-
+import PaqueteLectura.GeneradorAleatorio;
 /**
  *
  * @author Usuario
  */
 public class Habitacion {
+    private double costoXnoche;
+    private boolean ocupada;
+    private Cliente cli;
     
+    //constructor
+    public Habitacion(){
+        costoXnoche = GeneradorAleatorio.generarDouble(8000);
+        ocupada = false;
+        cli = null;
+    }
+    
+    public boolean estaOcupada(){
+        return ocupada;
+    }
+    
+    public void aumentarPrecio(double monto){
+        costoXnoche = monto;
+    }
+    
+    public void ingresoCliente(Cliente unCli){
+        cli = unCli;
+    }
+    
+    @Override
+    public String toString(){
+        String aux = " Costo: " + costoXnoche + " libre: " + estaOcupada();  
+        if(estaOcupada()){
+            aux = aux + " cliente: " + cli.toString();
+        }
+        return aux;
+    }
 }
