856a2027a3ae52f491de0a5075f8dfbcf6d7fbd3
Gaston <gastondiaz.lp@gmail.com>
2024-11-07 14:05:17 -0300

    concurrente p2

---
+++ 2-Objetos/Practica 1/Practica-2024/src/practica4/Figura.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica4;
+
+/**
+ *
+ * @author Usuario
+ */
+public abstract class Figura {
+    private String relleno;
+    private String linea;
+    
+        
+    
+}
